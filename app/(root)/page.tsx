a97078d89ff031075b84ee1257e6fd1562024c10
husen ikary <husenikary@mail.ru>
2025-05-13 20:52:07 +0300

    new block of video added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
 import React from 'react'
 
 const Page = () => {
   return (
     <main className='wrapper page'>
       <Header title='All videos' subHeader='Public Library' />
-      <h1 className='text-2xl font-karla'>Welcome to loom clone</h1>
+
+      <VideoCard 
+      id="1"
+      title="husen" 
+      thumbnail="/assets/samples/thumbnail (1).png" 
+      createdAt={new Date("2020-02-10 16:34:34") }
+      userImg="/assets/images/dummy.jpg" 
+      username="husen"
+      views={10} 
+      visibility="public" 
+      duration={156} 
+      />
     </main>
   )
 }
---
+++ components/VideoCard.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const VideoCard = ({
+  id,
+  title,
+  thumbnail,
+  createdAt,
+  userImg,
+  username,
+  views,
+  visibility,
+  duration,
+}: VideoCardProps) => {
+  return (
+    <Link href={`/video/${id}`} className='video-card'>
+      <Image src={thumbnail} alt='thumbnail' width={290} height={190} className='thumbnail' />
+      <article>
+        <div>
+          <figure>
+            <Image src={userImg} alt='userImg' width={34} height={34} className='rounded-full aspect-square' />
+            <figcaption>
+              <h3>{username}</h3>
+              <p>{visibility}</p>
+            </figcaption>
+          </figure>
+            <aside className=''>
+              <Image src='/assets/icons/eye.svg' alt='eye' width={16} height={16} />
+              <span>{views}</span>
+            </aside>
+        </div>
+        <h2>
+          {title} - {" "} {createdAt.toLocaleDateString("en-US", {
+            year: 'numeric',
+            month: 'short',
+            day: "numeric"
+          })}
+        </h2>
+        <button onClick={() => {}} className='copy-btn'>
+          <Image src={'/assets/icons/link.svg'} alt='link' width={18} height={18}/>
+        </button>
+        {duration && (
+          <div className='duration'>
+            {Math.ceil(duration / 60)}min
+          </div>
+        )}
+
+      </article>
+    </Link>
+  )
+}
+
+export default VideoCard
